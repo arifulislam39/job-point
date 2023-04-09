74f361d7fed04f74052ed4d4eded36a5b2f78d96
ariful-islam <arifulislam204909@gmail.com>
2023-04-10 01:49:52 +0600

    header section added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-           
+        <div className='flex justify-between ml-56 me-56 mt-5 items-center'>
+            <div className=''>
+                <h1 className='font-bold text-5xl'>One Step Closer To Your <span className='text-indigo-500'>Dream Job</span></h1>
+                <p className='mt-5'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                <button class="bg-indigo-500 rounded-lg p-3 text-white mt-4">
+                    Get Started
+                </button>
+            </div>
+            <div>
+                <img src="/src/assets/images/profile.png" alt="" />
+
+            </div>
 
         </div>
     );
