6e2b4462798f3d7461e141c6e8c766b7ca64a821
ariful-islam <arifulislam204909@gmail.com>
2023-04-11 21:02:26 +0600

    statistics radar chart added with assignment marks

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
 
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+
+
+
+
 const Statistics = () => {
+
+    const assignmentMark = [
+        {
+            assignment: 'Assignment: 1',
+            mark: 57,
+        },
+        {
+            assignment: 'Assignment: 2',
+            mark: 59,
+        },
+        {
+            assignment: 'Assignment: 3',
+            mark: 46,
+        },
+        {
+            assignment: 'Assignment: 4',
+            mark: 57,
+        },
+        {
+            assignment: 'Assignment: 5',
+            mark: 51,
+        },
+        {
+            assignment: 'Assignment: 6',
+            mark: 30,
+        },
+        {
+            assignment: 'Assignment: 7',
+            mark: 60,
+        },
+        {
+            assignment: 'Assignment: 8',
+            mark: 58,
+        },
+    ];
+
     return (
-        <div>
-            <h2>from statistics</h2>
+        <div className='ml-56 me-56 text-center mt-10'>
+
+            <RadarChart
+                data={assignmentMark}
+                width={1000}
+                height={500}
+                cx="50%" cy="50%" outerRadius="80%">
+                <PolarGrid />
+                <PolarAngleAxis dataKey="assignment" />
+                <PolarRadiusAxis />
+                <Radar
+                    dataKey="mark" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}
+
+                    name="Mike"></Radar>
+            </RadarChart>
+
         </div>
     );
 };
