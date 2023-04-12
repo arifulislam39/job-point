4ce8523a30d9b0958e166447e9757d42b0200528
ariful-islam <arifulislam204909@gmail.com>
2023-04-12 19:12:29 +0600

    updated blog questions

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -6,14 +6,23 @@ const Blog = () => {
             <h1 className='font-bold mt-10 text-3xl'>Blog Questions:</h1>
             <hr className='border-zinc-950' />
 
-            <h3 className='font-bold mt-5'>1. When should you use context API?</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, tempore ipsa quod nemo animi quia!</p>
-            <h3 className='font-bold mt-5'>2. What is a custom hook?</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, tempore ipsa quod nemo animi quia!</p>
+            <h3 className='font-bold mt-5'>1. What is a custom hook?</h3>
+            <p>A custom hook is a special JavaScript function whose name starts with ‘use’ and can be used to call other hooks.
+                <br />
+                Custom React JS hooks are reusable functions that a React JS software developer can use to add special and unique functionality to the React applications.</p>
+
+            <h3 className='font-bold mt-5'>2. When should you use context API?</h3>
+            <p>Context provides a way to pass data through the component tree without having to pass props down manually at every level.
+                <br />
+                Context API in React is to share data or the app state across all the app components. This means that the props can be accessed from any component. Therefore, no need to pâss props through other components to send it at the end to our destination component. Less code, less debugging hassle, and more code visibility.
+            </p>
+
             <h3 className='font-bold mt-5'>3. What is useRef?</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, tempore ipsa quod nemo animi quia!</p>
+            <p>useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference (aka ref). A reference is an object having a special property current.reference.
+                <br />
+                current accesses the reference value, and reference.current = newValue updates the reference value. Pretty simple.</p>
             <h3 className='font-bold mt-5'>4. What is useMemo?</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, tempore ipsa quod nemo animi quia!</p>
+            <p>useMemo is a React Hook that lets you cache the result of a calculation between re-renders.</p>
         </div>
     );
 };
