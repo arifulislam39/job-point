520a710e1da8489ff5e78b67722d5181b5126807
ariful-islam <arifulislam204909@gmail.com>
2023-04-12 16:09:04 +0600

    see all job btn done

--- src/components/FeaturedJob.jsx
+++ src/components/FeaturedJob.jsx
@@ -7,8 +7,12 @@ import { addToDb } from '../utilities/fakeDb';
 const FeaturedJob = () => {
 
     const [jobs, setJobs] = useState([]);
-   //console.log(jobs)
+    const [showAll, setShowAll] = useState(false);
+    //console.log(jobs)
 
+    const hnadleShowAll = () => {
+        setShowAll(true);
+    };
 
 
     useEffect(() => {
@@ -21,37 +25,44 @@ const FeaturedJob = () => {
             <h1 className='font-bold text-3xl mb-3'>Job Category List</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className='grid gap-6 lg:grid-cols-2 sm:grid-cols-1 mt-10'>
-                {
-                    jobs.map(job => <div className='text-left border-2 w-auto h-96 rounded-md p-6'
-                        key={job.id}>
-                        <img className='w-36 h-20 mb-5' src={job.img_logo} alt="" />
-                        <h2>{job.job_title}</h2>
-                        <h3>{job.company_name}</h3>
-                        <div className='flex gap-3 text-blue-700 mt-3'>
-                            <button className='border-2 rounded p-1'>{job.job_type_onsite}</button>
-                            <button className='border-2 rounded p-1'>{job.job_type_remote}</button>
-                        </div>
-                        <div className='flex gap-6 mt-3'>
-                            <div className='flex items-center'><img src={job.location_logo} alt="" />
-                                <p>{job.location}</p></div>
+                {jobs?.slice(0, showAll ? 6 : 4).map(job => <div className='text-left border-2 w-auto h-96 rounded-md p-6'
+                    key={job.id}>
+                    <img className='w-36 h-20 mb-5' src={job.img_logo} alt="" />
+                    <h2>{job.job_title}</h2>
+                    <h3>{job.company_name}</h3>
+                    <div className='flex gap-3 text-blue-700 mt-3'>
+                        <button className='border-2 rounded p-1'>{job.job_type_onsite}</button>
+                        <button className='border-2 rounded p-1'>{job.job_type_remote}</button>
+                    </div>
+                    <div className='flex gap-6 mt-3'>
+                        <div className='flex items-center'><img src={job.location_logo} alt="" />
+                            <p>{job.location}</p></div>
 
-                            <div className='flex items-center'><img src={job.salary_logo} alt="" />
-                                <p>{job.salary}</p></div>
-                        </div>
-
-
-                        <Link to={`/jobdetaile/${job.id}`}><button class="bg-indigo-500 rounded-lg p-3 text-white mt-3">
-                            View Details
-                        </button></Link>
+                        <div className='flex items-center'><img src={job.salary_logo} alt="" />
+                            <p>{job.salary}</p></div>
+                    </div>
 
 
-                    </div>)
+                    <Link to={`/jobdetaile/${job.id}`}><button class="bg-indigo-500 rounded-lg p-3 text-white mt-3">
+                        View Details
+                    </button></Link>
+
+
+                </div>)
 
                 }
             </div>
 
 
 
+
+            {!showAll && (
+                <span onClick={hnadleShowAll}>
+                    <button class="bg-indigo-500 rounded-lg p-3 text-white mt-5 mb-10">
+                        See All Jobs
+                    </button>
+                </span>
+            )}
 
         </div>
     );
