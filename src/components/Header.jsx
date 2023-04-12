45b685c3ff0e4131dec4bea41f848d0e65c7f5b6
ariful-islam <arifulislam204909@gmail.com>
2023-04-12 22:07:14 +0600

    img updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='flex justify-between ml-56 me-56 mt-5 items-center'>
+        <div className='flex justify-between ml-56 me-56 mt-5 items-center sm:grid-cols-1'>
             <div className=''>
                 <h1 className='font-bold text-5xl'>One Step Closer To Your <span className='text-indigo-500'>Dream Job</span></h1>
                 <p className='mt-5'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
